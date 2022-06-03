950afaacc21f464ee235c7d1e8597b313f52ef25
Martin Lemanski <martin.lemanski@gmx.at>
2022-06-04 00:03:02 +0200

    initial commit

---
+++ zomboid/talking-zombies/src/main/java/at/mlem/twitch/talkingenemies/zomboid/talkingzombies/Main.java
@@ -0,0 +1,7 @@
+package at.mlem.twitch.talkingenemies.zomboid.talkingzombies;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
