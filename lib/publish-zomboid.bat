56be45eb1429e9c7655598e59e6ea2f4536f9254
Martin Lemanski <martin.lemanski@gmx.at>
2022-09-13 10:54:12 +0200

    update pz version in push script

--- lib/publish-zomboid.bat
+++ lib/publish-zomboid.bat
@@ -1,3 +1,3 @@
 
 @rem used to publish the zomboid classes as jar file to your local maven repository
-mvn install:install-file -Dfile=zomboid.jar -DgroupId=zomboid -DartifactId=zomboid -Dversion=41.71 -Dpackaging=jar
+mvn install:install-file -Dfile=zomboid.jar -DgroupId=zomboid -DartifactId=zomboid -Dversion=41.76 -Dpackaging=jar
